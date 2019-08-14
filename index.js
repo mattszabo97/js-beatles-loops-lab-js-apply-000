d6838ded6fb540b2bb1b2486960d1f045055db0d
Matthew Szabo <mattmszabo@gmail.com>
2019-08-14 17:09:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ function johnLennonFacts(){}
 function theBeatlesPlay(musicians, instruments){
   let x=[]
   for(let i = 0; i < musicians.length; i++){
-    
-  }
+    let temp = musicians[i] + 'plays' + instuments[i].toLowerCase()
+    x.push(temp)
+  } return x
 }
 function iLoveTheBeatles(){}
