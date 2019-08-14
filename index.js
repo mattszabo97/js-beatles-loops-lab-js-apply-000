ecbc773cab9989b870357df773232e16da530720
Matthew Szabo <mattmszabo@gmail.com>
2019-08-14 17:12:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function johnLennonFacts(){}
 function theBeatlesPlay(musicians, instruments){
   let x=[]
   for(let i = 0; i < musicians.length; i++){
-    let temp = musicians[i] + 'plays' + instruments[i].toLowerCase()
+    let temp = musicians[i] + ' plays ' + instruments[i]
     x.push(temp)
   } return x
 }
